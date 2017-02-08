db7364014bd1c702cf0a9cc55cb079e3795a341c
hsmoothstone <hector.robinson@live.ca>
2017-02-08 13:42:04 -0500

    started to edit runge-kutta

--- runge_kutta.py
+++ runge_kutta.py
@@ -1,4 +1,4 @@
-def RungeKutta(r_start,r_end,h,P0,T0,P,M,L,T,stop,data):
+def RungeKutta(r_start,r_end,h,P0,T0,P_func,M_func,L_func,T_func,stop,data):
 	
 	r = np.arange(r_start,r_end,h)
 	P = np.arange(r_start,r_end,h)
@@ -13,17 +13,17 @@ def RungeKutta(r_start,r_end,h,P0,T0,P,M,L,T,stop,data):
 	
 	for i in range(0,len(r)-1):
 
-	    k1 = f(x[i],y[i],z[i])*h 
-	    l1 = g(x[i],y[i],z[i])*h
+	    k1 = P(r[i],P[i],T[i])*h 
+	    l1 = M(r[i],y[i],z[i])*h
 
-	    k2 = f(x[i]+h/2.0,y[i]+k1/2.0,z[i]+l1/2.0)*h 
-	    l2 = g(x[i]+h/2.0,y[i]+k1/2.0,z[i]+l1/2.0)*h
+	    k2 = P(r[i]+h/2.0,P[i]+k1/2.0,z[i]+l1/2.0)*h 
+	    l2 = M(r[i]+h/2.0,y[i]+k1/2.0,z[i]+l1/2.0)*h
 
-	    k3 = f(x[i]+h/2.0,y[i]+k2/2.0,z[i]+l2/2.0)*h 
-	    l3 = g(x[i]+h/2.0,y[i]+k2/2.0,z[i]+l2/2.0)*h
+	    k3 = P(r[i]+h/2.0,y[i]+k2/2.0,z[i]+l2/2.0)*h 
+	    l3 = M(r[i]+h/2.0,y[i]+k2/2.0,z[i]+l2/2.0)*h
 
-	    k4 = f(x[i]+h,y[i]+k3,z[i]+l3)*h 
-	    l4 = g(x[i]+h,y[i]+k3,z[i]+l3)*h
+	    k4 = P(r[i]+h,y[i]+k3,z[i]+l3)*h 
+	    l4 = M(r[i]+h,y[i]+k3,z[i]+l3)*h
 
 	    z[i+1] = z[i] + l1/6.0 + l2/3.0 + l3/3.0 + l4/6.0
 	    y[i+1] = y[i] + k1/6.0 + k2/3.0 + k3/3.0 + k4/6.0
