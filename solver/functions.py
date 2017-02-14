8633294e87ea06edfbb6f085d511b1d0598163dd
Brady Metherall <brady.metherall@uoit.net>
2017-02-13 19:30:08 -0500

    Updated main.py and functions.py to use the initialize file.

--- solver/functions.py
+++ solver/functions.py
@@ -1,52 +1,25 @@
-# I think I have done them incorrectly
-# perhaps we even need another main function that includes this and runge_kutta to do the work
-
-import scipy.constants
 import numpy as np
-
-#X = 1
-#Y = 1
-#Z = 1
-# WE'RE GOING TO NEED TO IMPORT X,Y,Z, AS WELL AS OTER INITIAL CONDITIONS
-# define these globally
-
-a = 7.565767 * 10 **(-16)
-gamma = 5./3.
-m_H = scipy.constants.m_p
-k = scipy.constants.k
-G = scipy.constants.G
-c = scipy.constants.c
-f_pp = 1.
-f_3a = 1.
-g_ff = 1.
-
-#mu = (2*X + 3./4. * Y + 1./2. * Z)**-1
-# define non-changing constants
-# these are defined globally
-
+import initialize as cons
 
 def T6(T):
-   return T / 10**6
+   return T / 10.0**6
 
 def T8(T):
-   return T / 10**8
+   return T / 10.0**8
 # define temperature constants
 
-
 def rho(P,T):
-   return (P - 1./3. * a * T**4) * mu * m_H / (k * T)
+   return (P - 1./3. * cons.a * T**4) * cons.mu * cons.m_H / (cons.k * T)
 # define rho (and therefore mu)
 
-
-
 def psi_pp(T):
-   return 1 + 1.412 * 10 ** 8 * (1.0/X - 1) * np.exp(-49.98 * T6(T) ** (-1./3.))
+   return 1 + 1.412 * 10.0 ** 8.0 * (1.0/cons.X - 1) * np.exp(-49.98 * T6(T) ** (-1./3.))
 
 def C_pp(T):
    return 1 + 0.0123 * T6(T)**(1./3.) + 0.0109 * T6(T)**(2./3.) + 0.000938 * T6(T)
 
 def e_pp(P,T):
-   return 0.241 * rho(P,T) * X**2 * f_pp * psi_pp(T) * C_pp(T) * T6(T)**(-2./3.) * np.exp(-33.8*T6(T)**(-1./3.))
+   return 0.241 * rho(P,T) * cons.X**2 * cons.f_pp * psi_pp(T) * C_pp(T) * T6(T)**(-2./3.) * np.exp(-33.8*T6(T)**(-1./3.))
 # define epsilon_pp (and therefore psi_pp * C_pp)
 
 def C_CNO(T):
@@ -54,42 +27,42 @@ def C_CNO(T):
 # define c_CNO
 
 def e_CNO(P,T):
-   return 8.67 * 10**20 * rho(P,T) * X * 0.5 * Z * C_CNO(T) * T6(T)**(-2./3.) * np.exp(-152.28*T6(T)**(-1./3.))
+   return 8.67 * 10**20 * rho(P,T) * cons.X * 0.5 * cons.Z * C_CNO(T) * T6(T)**(-2./3.) * np.exp(-152.28*T6(T)**(-1./3.))
 # define epsilon_CNO
 
 def e_3a(P,T):
-   return 50.9 * rho(P,T)**2 * Y**3 * T8(T)**-3 * f_3a * np.exp(-44.027 * T8(T)**-1)
+   return 50.9 * rho(P,T)**2 * cons.Y**3 * T8(T)**-3 * cons.f_3a * np.exp(-44.027 * T8(T)**-1)
 # define epsilon_3alpha
 
 def e(P,T):
-   return e_pp(P,T) + e_CNO(T) + e_3a(P,T)
+   return e_pp(P,T) + e_CNO(P,T) + e_3a(P,T)
 # define epsilon overall
 
 
 
 def tdivgbf(P,T):
-   return 0.708*(rho(P,T)*(1+X))**(1./5.)
+   return 0.708*(rho(P,T)*(1+cons.X))**(1./5.)
 
 def k_bf(P,T):
-   return 4.34*10**21 * tdivgbf(P,T)**-1 * Z * (1+X) * rho(P,T) / T**3.5
+   return 4.34*10**21 * tdivgbf(P,T)**-1 * Z * (1+cons.X) * rho(P,T) / T**3.5
 # define kappa_bf (and therefore t/g_bf)
 
 def k_ff(P,T):
-   return 3.68*10**18 * g_ff * (1 - Z) * (1 + X) * rho(P,T) / T**3.5
+   return 3.68*10**18 * cons.g_ff * (1 - cons.Z) * (1 + cons.X) * rho(P,T) / T**3.5
 # define kappa_ff
 
 def k_es():
-   return 0.02 * (1 + X)
+   return 0.02 * (1 + cons.X)
 
 def k_H(P,T):
-   return 7.9*10**-34 * (Z / 0.02) * rho(P,T)**(1./2.) * T**9
+   return 7.9*10**-34 * (cons.Z / 0.02) * rho(P,T)**(1./2.) * T**9
 # define kappa_H
 
 def kappa(P,T):
    if (3000 <= T <= 6000) and (10**-7 <= rho(P,T) <= 10**-2) and (0.001 < Z < 0.003):
-      return k_bf(P,T) + k_ff(P,T) + k_es + k_H(P,T)
+      return k_bf(P,T) + k_ff(P,T) + k_es() + k_H(P,T)
    else:
-      return k_bf(P,T) + k_ff(P,T) + k_es
+      return k_bf(P,T) + k_ff(P,T) + k_es()
 # define kappa overall
 
 
@@ -99,7 +72,7 @@ def dM(r,T,P,L):
    return 4 * np.pi * r**2 * rho(P,T)
 
 def dP(r,T,M,L):
-   return -G * M * rho(P,T) / r**2
+   return -cons.G * M * rho(P,T) / r**2
 
 def dL(r,T,M,P):
    return 4 * np.pi * r**2 * rho(P,T) * e(P,T)
@@ -107,11 +80,11 @@ def dL(r,T,M,P):
 # The left side of this equation needs to be a derivative
 # But I'm not sure the best way to do so
 def dT(r,P,M,L):
-   if (log(P) / log(T) < (gamma / (gamma - 1.))):
-      return -3. * kappa(P,T) * rho(P,T) * L / (4 * a * c * T**3 * 4 * np.pi * r**2)
+   if (log(P) / log(T) < (cons.gamma / (cons.gamma - 1.))):
+      return -3. * kappa(P,T) * rho(P,T) * L / (4 * cons.a * cons.c * T**3 * 4 * np.pi * r**2)
    # radiative
    else:
-      return -(1-1./gamma) * mu * m_H * G * M / (k * r**2)
+      return -(1-1./cons.gamma) * cons.mu * cons.m_H * cons.G * M / (cons.k * r**2)
    # adiabatic convection
 
 
--- solver/main.py
+++ solver/main.py
@@ -1,11 +1,10 @@
 #Main function for our program
 from runge_kutta import RungeKutta as RK
 from functions import *
+import initialize
 
-#composition components of star
-X = 1
-Y = 1
-Z = 1
+#Define all our constants. This is where we pass X, Y, Z, and it should work everywhere
+initialize.init(0.7,0.25,0.05)
 
 #Taking zero to be the center of the star
 #initial pressure at center of star
@@ -21,15 +20,15 @@ rho0=1.0
 #final r value, file should end though with stop function
 r_end = 10.0
 
-#now need to pass mu to functions within function.py
 #Define function mu that is components of star
-mu = (2*X + 3./4. * Y + 1./2. * Z)**-1
 
 def stop(P,M,L,T,data):
 	if (P < data or M<data or L<data or T<data):
 		return True
 	else:
-		return False		
+		return False
+
+#print e(10.0,10.0**7) #Testing some stuff	
 
 RK(0.0, r_end, 0.01, P0, M0, L0, T0, rho0, dP, dM, dL, dT, stop, 0.00001)
 
